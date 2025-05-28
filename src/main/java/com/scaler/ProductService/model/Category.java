8d1d1340367cc2496a1f646b4119f42295005d62
unknown <utkarshsanju13gmail.com>
2025-05-28 11:00:39 +0530

    flyway

--- src/main/java/com/scaler/ProductService/model/Category.java
+++ src/main/java/com/scaler/ProductService/model/Category.java
@@ -1,11 +1,17 @@
 package com.scaler.ProductService.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
+import java.util.List;
 
 @Entity(name = "categories")
 public class Category extends BaseModel {
 
     private  String title;
+
+    @OneToMany(mappedBy = "category")
+    private List<Product> listOfProduct;
 
     public String getTitle() {
         return title;
